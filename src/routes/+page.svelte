5d26ba519bb4b56786e28eb6c1da7e1d3681e16f
nicolatl <77587217+nicolatl@users.noreply.github.com>
2026-02-16 13:08:35 -0500

    Step 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Nicola</h1>
+<nav>
+    <a class="current" href=".">Home</a>
+    <a href="projects">Projects</a>
+    <a href="contact">Contact</a>
+    <a href="resume">Resume</a>
+    <a href="https://github.com/nicolatl" target="_blank">GitHub</a>
+</nav>
+<p>I am a masters student from Hamilton, Canada currently living in Cambridge, USA.</p>
+<img src="images/nicola.jpeg" width="500" alt="Black-and-white photo of Nicola smiling and holding an umbrella in front of a cat">
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,31 @@
 <svelte:head>
   <title>Contact me</title>
 </svelte:head>
+
+<h1>Contact</h1>
+<nav>
+    <a href=".">Home</a>
+    <a href="projects">Projects</a>
+    <a class="current" href="contact">Contact</a>
+    <a href="resume">Resume</a>
+    <a href="https://github.com/nicolatl" target="_blank">GitHub</a>
+</nav>
+Please do not contact me in this way.
+<form action="mailto:77587217+nicolatl@users.noreply.github.com" method="GET">
+    <label>
+        Email:
+        <input type="email" name="email" required>
+    </label>
+    <br>
+    <label>
+        Subject:
+        <input name="subject" required>
+    </label>
+    <br>
+    <label>
+        Message:
+        <textarea name="body" required></textarea>
+    </label>
+    <br>
+    <button>Send</button>
+</form>
