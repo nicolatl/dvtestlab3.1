ff1f983f18c6f662f97a85a67d04395f70d83279
nicolatl <77587217+nicolatl@users.noreply.github.com>
2026-03-11 20:03:30 -0400

    lab 5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,29 @@
 <script>
-  import projects from "$lib/projects.json";
-  import Project from "$lib/Project.svelte";
-  import books from "$lib/reading.json";
-  import ReadingItem from "$lib/ReadingItem.svelte";
+    import projects from "$lib/projects.json";
+    import Project from "$lib/Project.svelte";
+    import books from "$lib/reading.json";
+    import ReadingItem from "$lib/ReadingItem.svelte";
+
+    import { onMount } from "svelte";
+
+    let githubData = null; // This will eventually hold our Github stats
+    let loading = true; // This will be true *until* the fetch's promise resolves to a value
+    let error = null; // If the API call resulted in an error, it will go into this variable
+
+    onMount(async () => {
+        try { // First, try running this block of code
+            console.log("Page has been mounted!")
+            let response = await fetch("https://api.github.com/users/nicolatl");
+            console.log(response);
+            githubData = await response.json();
+            console.log(githubData);
+        } catch (err) { // if the "try" block runs into an error, cancel excecution and run this code instead
+            error = err;
+        }
+        loading = false; // don't forget to add this line!
+    })
+
+
 </script>
 
 <h1>Nicola</h1>
@@ -20,6 +41,24 @@
         {/each}
     </div>
 </div>
+<section>
+    <h2>GitHub Stats</h2>
+    {#if loading}
+        <p>Loading...</p>
+    {:else if error}
+        <p>Something went wrong: {error.message}</p>
+    {:else}
+        <dl>
+            <dt>Followers</dt>
+            <dd>{githubData.followers}</dd>
+            <dt>Following</dt>
+            <dd>{githubData.following}</dd>
+            <dt>Public Repositories</dt>
+            <dd>{githubData.public_repos}</dd>
+        </dl>
+    {/if}
+
+</section>
 
 <h2>Latest projects</h2>
 <div class="projects">
@@ -43,5 +82,14 @@
     }
     .about {
         max-width: 500px;
+    }
+    dl {
+        display: grid;
+    }
+    dt {
+        grid-row: 1;
+    }
+    dd {
+        margin:0;
     }
 </style>
